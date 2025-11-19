c604d88c98f5265231bc4841be18bb48047d0802
ParkYeongseo <86962788+SkylightStudio07@users.noreply.github.com>
2025-11-19 21:33:09 +0900

    64. 멤버 함수

--- Source/ObstacleAssault/MovingPlatform.h
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -23,6 +23,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
 	UPROPERTY(EditAnywhere, Category="Moving Platform")
 	FVector PlatformVelocity = FVector(100, 0, 0);
 
@@ -34,4 +35,6 @@ public:
 	
 	FVector StartLocation;
 
+	void MovePlatform(float DeltaTime);
+	void RotatePlatform(float DeltaTime);
 };
